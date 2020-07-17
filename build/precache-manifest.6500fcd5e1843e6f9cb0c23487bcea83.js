2f30623b3e3f15060279f9220bc6e9121f7f5d9c
abdullah <hbkamk@gmail.com>
2020-07-17 19:32:47 +0600

    deplo

---
+++ build/precache-manifest.6500fcd5e1843e6f9cb0c23487bcea83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb10edbf920e571fc982a6d862447d7f",
+    "url": "/Movie-Search-App/index.html"
+  },
+  {
+    "revision": "ff794592578c91020739",
+    "url": "/Movie-Search-App/static/css/main.46673b38.chunk.css"
+  },
+  {
+    "revision": "b020aa932c06032e1ead",
+    "url": "/Movie-Search-App/static/js/2.e0795e11.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Movie-Search-App/static/js/2.e0795e11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff794592578c91020739",
+    "url": "/Movie-Search-App/static/js/main.3a47d725.chunk.js"
+  },
+  {
+    "revision": "d027b3aacdb6c35c3036",
+    "url": "/Movie-Search-App/static/js/runtime-main.b8196268.js"
+  }
+]);
